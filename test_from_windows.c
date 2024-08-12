1b7199c8393fac7730aee8b40572f2183a02a2e4
@li-peixiansang <2766988668@qq.com>
2024-08-12 10:45:09 +0800

    modify from windows

--- test_from_windows.c
+++ test_from_windows.c
@@ -3,5 +3,12 @@
 int main()
 {
 	printf("hello github, from windows");
+	//add from window
+	
+	printf("hello github, from windows");
+	printf("hello github, from windows");
+	printf("hello github, from windows");
+	printf("hello github, from windows");
+	printf("hello github, from windows");
 	return 0;
 }
